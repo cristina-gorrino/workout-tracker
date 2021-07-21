409c6acade8ec92c6559a484a69b285ef7696542
cgorrino <cgorrino03@gmail.com>
2021-07-20 22:20:49 -0700

    Adding api routes and correcting mongoose db connection

---
+++ routes/api.js
@@ -0,0 +1,48 @@
+const router = require("express").Router();
+const {Workout} = require("../models");
+
+
+// Get the most recent workout
+router.get("/api/workouts", (req, res) => {
+  Workout.find({})
+    .sort({ day: -1 })
+    .limit(1)
+    .then(dbWorkout => {
+        console.log(dbWorkout)
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+// Add Exercise
+router.put('/api/workouts/:id', (req, res) => {
+
+})
+
+// Create Workout
+router.post("/api/workout", ({ body }, res) => {
+  Workout.create(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+// Get Workouts in range (the last 7 workouts)
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find({})
+      .sort({ day: -1 })
+        .limit(7)
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+
+module.exports = router;
