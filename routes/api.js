2181948fb363a4adc8a2af4fc3d6aab3b3accf6c
cgorrino <cgorrino03@gmail.com>
2021-07-21 12:44:46 -0700

    Modifying route for workouts in a given range. Adding total duration field

--- routes/api.js
+++ routes/api.js
@@ -34,15 +34,32 @@ router.post("/api/workout", ({ body }, res) => {
 
 // Get Workouts in range (the last 7 workouts)
 router.get("/api/workouts/range", (req, res) => {
-    Workout.find({})
-      .sort({ day: -1 })
-        .limit(7)
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: {$sum: "$exercises.duration"}
+            }
+        }
+    ])
       .then(dbWorkout => {
+          console.log(dbWorkout)
         res.json(dbWorkout);
       })
       .catch(err => {
         res.status(400).json(err);
       });
   });
+
+//   router.get("/api/workouts/range", (req, res) => {
+//     Workout.find({})
+//       .sort({ day: -1 })
+//         .limit(7)
+//       .then(dbWorkout => {
+//         res.json(dbWorkout);
+//       })
+//       .catch(err => {
+//         res.status(400).json(err);
+//       });
+//   });
 
 module.exports = router;
